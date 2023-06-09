d6150a4a174500b096c2adce7c99c56ac04fabb1
Rohan Faiyaz Khan <rohankhan170@gmail.com>
2023-06-09 17:08:04 -0400

    Copied over dist from production commit 77872a0a

---
+++ css/README.md
@@ -0,0 +1,6 @@
+# css
+Where to add/edit css - as a submodule
+
+Use this file directory if you need to just have access to the main CSS files. Use the production repository https://github.com/TMPortal/production if you are running a local server on your machine and view the site in production. 
+
+Run git submodule init and git submodule update
---
+++ js/index.js
@@ -0,0 +1,39 @@
+const ELEMENT_IDS = {
+    BROWSE: "btn-browse",
+    NAVBROWSE: "nav-browse",
+};
+
+// const browseButton = document.getElementById(ELEMENT_IDS.BROWSE);
+// const navBrowse = document.getElementById(ELEMENT_IDS.NAVBROWSE);
+
+// function isDropDownOpen() {
+//     return navBrowse.classList.contains("open");
+// }
+
+// function openDropDown() {
+//     navBrowse.classList.add("open");
+//     window.addEventListener("click", closeDropdownOnOutsideClick);
+// }
+
+// function closeDropDown() {
+//     navBrowse.classList.remove("open");
+//     window.removeEventListener("click", closeDropdownOnOutsideClick);
+// }
+
+// function closeDropdownOnOutsideClick(event) {
+//     if (isDropDownOpen()) {
+//         if (!navBrowse.contains(event.target)) {
+//             closeDropDown();
+//         }
+//     }
+// }
+
+// function toggleBrowseDropDown() {
+//     if (isDropDownOpen()) {
+//         closeDropDown();
+//     } else {
+//         openDropDown();
+//     }
+// }
+
+// browseButton.addEventListener("click", toggleBrowseDropDown);
